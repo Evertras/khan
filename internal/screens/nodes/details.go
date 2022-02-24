ddf64ef1aac68d07d63880a5622adac8e83d98a3
Brandon Fulljames <bfullj@gmail.com>
2022-02-24 21:16:36 +0900

    Remove old code

--- internal/screens/nodes/details.go
+++ internal/screens/nodes/details.go
@@ -1,12 +1,7 @@
 package nodes
 
 import (
-	"fmt"
-	"strings"
-
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/evertras/khan/internal/keyvalsort"
-	"github.com/evertras/khan/internal/styles"
 )
 
 func (m Model) updateDetails(msg tea.Msg) (Model, tea.Cmd) {
@@ -27,46 +22,4 @@ func (m Model) updateDetails(msg tea.Msg) (Model, tea.Cmd) {
 
 func (m Model) viewDetails() string {
 	return m.detailsDataTree.View()
-}
-
-func (m Model) viewDetailsOld() string {
-	d := m.details
-
-	body := strings.Builder{}
-
-	l := func(k, v string) {
-		body.WriteString(styles.Key.Render(k+": ") + v + "\n")
-	}
-
-	l("id", d.ID)
-	l("name", d.Name)
-	l("status", d.Status)
-	l("elgbl", d.SchedulingEligibility)
-	l("drain", fmt.Sprintf("%v", d.Drain))
-	l("dc", d.Datacenter)
-	l("addr", d.HTTPAddr)
-
-	l("devices", "")
-
-	for _, device := range d.NodeResources.Devices {
-		l("  - id", device.ID())
-
-		count := len(device.Instances)
-
-		if count > 1 {
-			l("    count:", fmt.Sprintf("%d", count))
-		}
-	}
-
-	l("meta", "")
-
-	sortedMetaVals := keyvalsort.SortedStringMapValues(d.Meta)
-
-	for _, kv := range sortedMetaVals {
-		l("  "+kv.Key, kv.Val)
-	}
-
-	body.WriteString("\n")
-
-	return body.String()
 }
