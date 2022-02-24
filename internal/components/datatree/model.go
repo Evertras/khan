4c07d515fe6d2d7de9bdea1b4476a892020a669b
Brandon Fulljames <bfullj@gmail.com>
2022-02-24 18:59:21 +0900

    Slightly saner newlines for now

--- internal/components/datatree/model.go
+++ internal/components/datatree/model.go
@@ -14,7 +14,7 @@ var (
 	styleFieldKey = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{
 		Light: "#224",
 		Dark:  "#b8e",
-	}).Bold(true)
+	}).Bold(true).MarginRight(1)
 )
 
 type Model struct {
@@ -40,6 +40,10 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 	return m, nil
 }
 
+func trimNewline(s string) string {
+	return strings.TrimSuffix(s, "\n")
+}
+
 func (m Model) renderDataNodeArray(data reflect.Value, indentLevel int) string {
 	result := strings.Builder{}
 
@@ -50,8 +54,7 @@ func (m Model) renderDataNodeArray(data reflect.Value, indentLevel int) string {
 	}
 
 	switch elemType.Kind() {
-
-	case reflect.Struct:
+	case reflect.Struct, reflect.Array, reflect.Slice:
 		result.WriteString("\n")
 		for i := 0; i < data.Len(); i++ {
 			style := lipgloss.NewStyle().
@@ -60,12 +63,12 @@ func (m Model) renderDataNodeArray(data reflect.Value, indentLevel int) string {
 				PaddingLeft(1).
 				PaddingRight(1)
 			entryStr := m.renderDataNode(data.Index(i), 0)
-			result.WriteString(style.Render(strings.TrimSuffix(entryStr, "\n")))
+			result.WriteString(style.Render(trimNewline(entryStr)))
 			result.WriteString("\n")
 		}
 
 	default:
-		result.WriteString(fmt.Sprintf(" %v\n", data))
+		result.WriteString(fmt.Sprintf("%v", data))
 	}
 
 	return result.String()
@@ -76,6 +79,8 @@ func (m Model) renderDataNodeStruct(data reflect.Value, indentLevel int) string 
 	indent := strings.Repeat(m.indentStr, indentLevel)
 
 	fieldNames := []string{}
+
+	result.WriteString("\n")
 
 	for i := 0; i < data.Type().NumField(); i++ {
 		field := data.Type().Field(i)
@@ -103,6 +108,10 @@ func (m Model) renderDataNodeStruct(data reflect.Value, indentLevel int) string 
 		result.WriteString(indent)
 		result.WriteString(styleFieldKey.Render(fieldName + ":"))
 
+		result.WriteString(m.renderDataNode(field, indentLevel+1))
+		result.WriteString("\n")
+		continue
+
 		switch field.Type().Kind() {
 		case reflect.Struct:
 			result.WriteString("\n")
@@ -128,13 +137,20 @@ func (m Model) renderDataNode(data reflect.Value, indentLevel int) string {
 		data = data.Elem()
 	}
 
+	var result string
+
 	switch data.Kind() {
 	case reflect.Struct:
-		return m.renderDataNodeStruct(data, indentLevel)
+		result = m.renderDataNodeStruct(data, indentLevel)
+
+	case reflect.Array, reflect.Slice:
+		result = m.renderDataNodeArray(data, indentLevel)
 
 	default:
-		return fmt.Sprintf("%v", data)
+		result = fmt.Sprintf("%v", data)
 	}
+
+	return trimNewline(result)
 }
 
 func (m Model) View() string {
@@ -142,7 +158,9 @@ func (m Model) View() string {
 
 	reflected := reflect.ValueOf(m.data)
 
-	body.WriteString(m.renderDataNode(reflected, 0))
+	rendered := m.renderDataNode(reflected, 0)
+
+	body.WriteString(strings.TrimSpace(rendered))
 
 	return body.String()
 }
